e4c0c52e5fcd3e4f7e90d9a9e78c3d71e37816cf
Ingvar Stepanyan <me@rreverser.com>
2017-01-31 20:35:13 +0000

    Remove unjson crate

--- crates/unjson/src/lib.rs
+++
@@ -1,8 +0,0 @@
-extern crate serde_json;
-
-mod unjson;
-pub mod error;
-pub mod result;
-pub mod ty;
-
-pub use unjson::*;
--- crates/unjson/src/result.rs
+++
@@ -1,4 +0,0 @@
-use std::result;
-use error::Error;
-
-pub type Result<T> = result::Result<T, Error>;
